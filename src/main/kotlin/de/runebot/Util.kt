5a180b76616f4d4061d5e48d6604bf11504dcaa3
Johannes Rappelt <mail@jayhannes.de>
2022-07-31 21:56:38 +0200

    add sendImage methods

--- src/main/kotlin/de/runebot/Util.kt
+++ src/main/kotlin/de/runebot/Util.kt
@@ -1,5 +1,13 @@
 package de.runebot
 
+import dev.kord.core.behavior.channel.MessageChannelBehavior
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.rest.NamedFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.random.Random
 
 object Util
@@ -9,5 +17,17 @@ object Util
         val stringBuilder = StringBuilder()
         this.forEach { stringBuilder.append(if (Random.nextBoolean()) it.uppercase() else it.lowercase()) }
         return stringBuilder.toString()
+    }
+
+    suspend fun sendImage(channel: MessageChannelBehavior, path: Path)
+    {
+        sendImage(channel, path.fileName.toString(), withContext(Dispatchers.IO) {
+            Files.newInputStream(path)
+        })
+    }
+
+    suspend fun sendImage(channel: MessageChannelBehavior, fileName: String, inputStream: InputStream)
+    {
+        channel.createMessage { this.files.add(NamedFile(fileName, inputStream)) }
     }
 }
