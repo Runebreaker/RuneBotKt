ea5c70d3b99b27dac5317a9e8e725876a632f56c
Johannes Rappelt <mail@jayhannes.de>
2022-08-21 15:12:08 +0200

    implemented DxDBehavior

---
+++ src/main/kotlin/de/runebot/behaviors/DxDBehavior.kt
@@ -0,0 +1,38 @@
+package de.runebot.behaviors
+
+import de.runebot.Util
+import dev.kord.core.event.message.MessageCreateEvent
+import info.debatty.java.stringsimilarity.JaroWinkler
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+object DxDBehavior : Behavior
+{
+    val lines: List<String>
+    val jaroWinkler = JaroWinkler()
+
+    init
+    {
+        DxDBehavior::class.java.getResourceAsStream("/HighSchoolDxD.txt").use { stream ->
+            InputStreamReader(stream!!, Charsets.UTF_8).use { inputReader ->
+                BufferedReader(inputReader).use { bufferedReader ->
+                    lines = bufferedReader.lines().toList().filter { it.length > 3 }
+                }
+            }
+        }
+    }
+
+    override suspend fun run(content: String, messageCreateEvent: MessageCreateEvent)
+    {
+        val results = mutableListOf<String>()
+
+        lines.forEachIndexed { index, s ->
+            if (jaroWinkler.similarity(s.lowercase(), content.lowercase()) > .75)
+            {
+                results.add(lines.getOrNull(index + 1) ?: return@forEachIndexed)
+            }
+        }
+
+        Util.sendMessage(messageCreateEvent, results.randomOrNull() ?: return)
+    }
+}
