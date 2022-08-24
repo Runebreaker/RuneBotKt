cced67d0fabdf5c4e9db94fdc14a9dfa34867b10
s361591 <gino.schulze-skyfire@web.de>
2022-08-24 17:37:37 +0200

    Added help text implementation.

--- src/main/kotlin/de/runebot/commands/CollectionCommand.kt
+++ src/main/kotlin/de/runebot/commands/CollectionCommand.kt
@@ -4,10 +4,15 @@ import de.runebot.Util
 import dev.kord.core.Kord
 import dev.kord.core.event.message.MessageCreateEvent
 
-object CollectionCommand: MessageCommand
+object CollectionCommand : MessageCommandInterface
 {
     override val names: List<String>
         get() = listOf("collection", "col", "c")
+    override val shortHelpText: String
+        get() = "collection functionality for use with mudae"
+    override val longHelpText: String
+        get() = "`$commandExample get key`: Gets stored config value for given key.\n" +
+                "`$commandExample set key value`: Sets config key to given value."
 
     override fun prepare(kord: Kord)
     {
