e3a0584b278456ffe54876f19b6a71be146c9164
s361591 <gino.schulze-skyfire@web.de>
2022-08-24 23:45:59 +0200

    Added help text with formatting from StringTree in Util

--- src/main/kotlin/de/runebot/commands/CollectionCommand.kt
+++ src/main/kotlin/de/runebot/commands/CollectionCommand.kt
@@ -1,6 +1,7 @@
 package de.runebot.commands
 
 import de.runebot.Util
+import de.runebot.database.DB
 import dev.kord.core.Kord
 import dev.kord.core.event.message.MessageCreateEvent
 
@@ -11,8 +12,28 @@ object CollectionCommand : MessageCommandInterface
     override val shortHelpText: String
         get() = "collection functionality for use with mudae"
     override val longHelpText: String
-        get() = "`$commandExample get key`: Gets stored config value for given key.\n" +
-                "`$commandExample set key value`: Sets config key to given value."
+        get()
+        {
+            return Util.StringTree(commandExample).getRoot()
+                .addChild("add")
+                .addChild("series <series name>                 - Adds series to collection.").moveUp()
+                .addChild("character <character name>           - Adds character to collection.").moveUp()
+                .addChild("combo <character name> <series name> - Adds combo to collection.").moveUp()
+                .moveUp()
+                .addChild("yeet")
+                .addChild("series <series name>                 - Removes series from collection.").moveUp()
+                .addChild("character <character name>           - Removes character from collection.").moveUp()
+                .addChild("combo <character name> <series name> - Removes combo from collection.").moveUp()
+                .moveUp()
+                .addChild("find")
+                .addChild("series <series name>                 - Tells, which users collect the specified series.").moveUp()
+                .addChild("character <character name>           - Tells, which users collect the specified character.").moveUp()
+                .addChild("combo <character name> <series name> - Tells, which users collect the specified combo.").moveUp()
+                .moveUp()
+                .addChild("show <mention>                        - Shows the collection of specified user.")
+                .moveToRoot()
+                .toString()
+        }
 
     override fun prepare(kord: Kord)
     {
@@ -23,8 +44,52 @@ object CollectionCommand : MessageCommandInterface
     {
         if (args.size <= 1)
         {
-            Util.sendMessage(event,"Try >(`${names.joinToString("` | `")}`) `help`")
+            Util.sendMessage(event, "Try >(`${names.joinToString("` | `")}`) `help`")
             return
+        }
+
+        event.message.author?.let {
+            when (args[1])
+            {
+                "add" ->
+                {
+                    when (args[2])
+                    {
+                        "series" ->
+                        {
+                            DB.addToCollection(it.id.value.toLong(), "All characters", args[2])
+                        }
+                        "character" ->
+                        {
+
+                        }
+                        "combo" ->
+                        {
+
+                        }
+                        else ->
+                        {
+
+                        }
+                    }
+                }
+                "yeet" ->
+                {
+
+                }
+                "find" ->
+                {
+
+                }
+                "show" ->
+                {
+
+                }
+                else ->
+                {
+                    Util.sendMessage(event, "Try >(`${names.joinToString("` | `")}`) `help`")
+                }
+            }
         }
     }
 }
