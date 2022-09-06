4d535135eacf192b2bf8c5c97e35d4e008b43c8a
s361591 <gino.schulze-skyfire@web.de>
2022-09-06 20:47:50 +0200

    Finished TagCommand.

--- src/main/kotlin/de/runebot/commands/TagCommand.kt
+++ src/main/kotlin/de/runebot/commands/TagCommand.kt
@@ -8,15 +8,71 @@ import dev.kord.core.event.message.MessageCreateEvent
 
 object TagCommand : MessageCommandInterface
 {
+    private val create = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(listOf("create", "cr", "c"), Pair("create <tag name> <message>", "Creates a new tag.")),
+        { event, args, _ ->
+            event.message.author?.let {
+                when (DB.storeTag(args[0], args.subList(1, args.size).joinToString(" "), it.id.value.toLong()))
+                {
+                    DBResponse.SUCCESS -> Util.sendMessage(event, "Tag created successfully.")
+                    else -> Util.sendMessage(event, "Tag couldn't be created.")
+                }
+            }
+        },
+        emptyList()
+    )
+    private val update = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(listOf("update", "up", "u"), Pair("update <tag name> <new message>", "Updates a tag, if you own it.")),
+        { event, args, _ ->
+            event.message.author?.let {
+                when (DB.updateTagIfOwner(args[0], args.subList(1, args.size).joinToString(" "), it.id.value.toLong()))
+                {
+                    DBResponse.SUCCESS -> Util.sendMessage(event, "Tag updated successfully.")
+                    DBResponse.FAILURE -> Util.sendMessage(event, "Tag couldn't be updated.")
+                    DBResponse.WRONG_USER -> Util.sendMessage(event, "You don't own this tag.")
+                    DBResponse.MISSING_ENTRY -> Util.sendMessage(event, "Tag doesn't exist.")
+                }
+            }
+        },
+        emptyList()
+    )
+    private val delete = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(listOf("delete", "del", "d"), Pair("delete <tag name>", "Deletes the tag, if you own it.")),
+        { event, args, _ ->
+            event.message.author?.let {
+                when (DB.deleteTagIfOwner(args.subList(0, args.size).joinToString(" "), it.id.value.toLong()))
+                {
+                    DBResponse.SUCCESS -> Util.sendMessage(event, "Tag deleted successfully.")
+                    DBResponse.FAILURE -> Util.sendMessage(event, "Tag couldn't be deleted.")
+                    DBResponse.WRONG_USER -> Util.sendMessage(event, "You don't own this tag.")
+                    DBResponse.MISSING_ENTRY -> Util.sendMessage(event, "Tag doesn't exist.")
+                }
+            }
+        },
+        emptyList()
+    )
+    private val tag = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(listOf("tag", "t"), Pair("tag <tag name>", "Outputs the tag stored under the specified name.")),
+        { event, args, _ ->
+            event.message.author?.let {
+                DB.getTag(args[0])?.let {
+                    Util.sendMessage(event, it)
+                } ?: Util.sendMessage(event, "Tag not found.")
+            }
+        },
+        listOf(
+            create,
+            update,
+            delete
+        )
+    )
+
     override val names: List<String>
         get() = listOf("tag", "t")
     override val shortHelpText: String
         get() = "create/get saved tags"
     override val longHelpText: String
-        get() = "`$commandExample tagName`: Display tag.\n" +
-                "`$commandExample create tagName tagContent`: Create tag.\n" +
-                "`$commandExample update tagName tagContent`: Update tag.\n" +
-                "`$commandExample delete tagName`: Delete tag."
+        get() = tag.toTree().toString()
     private lateinit var kord: Kord
 
     override fun prepare(kord: Kord)
@@ -27,150 +83,12 @@ object TagCommand : MessageCommandInterface
 
     override suspend fun execute(event: MessageCreateEvent, args: List<String>)
     {
-        val creatorId = event.message.author?.id?.value
-        if (creatorId == null)
-        {
-            Util.sendMessage(event, "Couldn't process request - Missing user ID")
-            return
-        }
         if (args.size <= 1)
         {
             Util.sendMessage(event, "Try >(`${names.joinToString("` | `")}`) `help`")
             return
         }
-        when (args[1])
-        {
-            "create" ->
-            {
-                if (args.size <= 2)
-                {
-                    sendTag(event, args[1])
-                    return
-                }
-                else if (args.size <= 3)
-                {
-                    Util.sendMessage(event, "Cannot create empty tag.")
-                    return
-                }
-                val tagCreateResponse = DB.storeTag(
-                    args[2],
-                    args.subList(3, args.size).joinToString(" "),
-                    event.message.author?.id?.value?.toLong() ?: 0
-                )
-                when (tagCreateResponse)
-                {
-                    DBResponse.SUCCESS ->
-                    {
-                        Util.sendMessage(event, "Tag successfully created.")
-                    }
 
-                    DBResponse.FAILURE ->
-                    {
-                        Util.sendMessage(event, "Tag could not be created.")
-                    }
-
-                    else ->
-                    {
-                        Util.sendMessage(event, "Something unexpected happened. Ping a RuneBot admin.")
-                    }
-                }
-            }
-
-            "update" ->
-            {
-                if (args.size <= 2)
-                {
-                    sendTag(event, args[1])
-                    return
-                }
-                else if (args.size <= 3)
-                {
-                    Util.sendMessage(event, "Cannot make tag empty.")
-                    return
-                }
-                val tagUpdateResponse = if (MessageCommandInterface.isAdmin(event)) DB.updateTag(
-                    args[2],
-                    args.subList(3, args.size).joinToString(" "),
-                    event.message.author?.id?.value?.toLong() ?: 0
-                )
-                else DB.updateTagIfOwner(
-                    args[2],
-                    args.subList(3, args.size).joinToString(" "),
-                    event.message.author?.id?.value?.toLong() ?: 0
-                )
-                when (tagUpdateResponse)
-                {
-                    DBResponse.SUCCESS ->
-                    {
-                        Util.sendMessage(event, "Tag successfully updated.")
-                    }
-
-                    DBResponse.FAILURE ->
-                    {
-                        Util.sendMessage(event, "Tag could not be updated.")
-                    }
-
-                    DBResponse.WRONG_USER ->
-                    {
-                        Util.sendMessage(event, "You do not own this tag.")
-                    }
-
-                    DBResponse.MISSING_ENTRY ->
-                    {
-                        Util.sendMessage(event, "Tag doesn't exist.")
-                    }
-                }
-            }
-
-            "delete" ->
-            {
-                if (args.size <= 2)
-                {
-                    sendTag(event, args[1])
-                    return
-                }
-                val tagDeleteResponse = if (MessageCommandInterface.isAdmin(event)) DB.deleteTag(
-                    args[2]
-                )
-                else DB.deleteTagIfOwner(
-                    args[2],
-                    creatorId.toLong()
-                )
-                when (tagDeleteResponse)
-                {
-                    DBResponse.SUCCESS ->
-                    {
-                        Util.sendMessage(event, "Tag successfully deleted.")
-                    }
-
-                    DBResponse.FAILURE ->
-                    {
-                        Util.sendMessage(event, "Tag could not be deleted.")
-                    }
-
-                    DBResponse.WRONG_USER ->
-                    {
-                        Util.sendMessage(event, "You do not own this tag.")
-                    }
-
-                    DBResponse.MISSING_ENTRY ->
-                    {
-                        Util.sendMessage(event, "Tag doesn't exist.")
-                    }
-                }
-            }
-
-            else ->
-            {
-                sendTag(event, args[1])
-            }
-        }
-    }
-
-    private suspend fun sendTag(event: MessageCreateEvent, tagName: String)
-    {
-        DB.getTag(tagName)?.let {
-            Util.sendMessage(event, it)
-        } ?: Util.sendMessage(event, "Tag doesn't exist.")
+        if (names.contains(args[0].substring(1))) tag.execute(event, args.subList(1, args.size), listOf(args[0].substring(1)))
     }
 }
