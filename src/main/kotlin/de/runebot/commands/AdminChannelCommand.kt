9a59704ed2784cd48a94dd71b6f1c43cfccd21b8
s361591 <gino.schulze-skyfire@web.de>
2022-09-07 17:34:57 +0200

    Implemented AdminChannel using Subcommand.

--- src/main/kotlin/de/runebot/commands/AdminChannelCommand.kt
+++ src/main/kotlin/de/runebot/commands/AdminChannelCommand.kt
@@ -9,13 +9,32 @@ import dev.kord.core.event.message.MessageCreateEvent
 
 object AdminChannelCommand : MessageCommandInterface
 {
+    private val set = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(listOf("set", "s"), Pair("set", "Sets the current channel as admin channel.")),
+        { event, args, _ ->
+            Config.store("adminChannel", event.message.channel.id.toString())
+            Util.sendMessage(event, "Admin channel set!")
+        },
+        emptyList()
+    )
+    private val adminchannel = MessageCommandInterface.Subcommand(
+        MessageCommandInterface.CommandDescription(names, Pair("adminchannel", "Sends a message in the currently set admin channel.")),
+        { event, args, _ ->
+            Config.get("adminChannel")?.let { channelID ->
+                Util.sendMessage(MessageChannelBehavior(Snowflake(channelID), kord), "This is the admin channel.")
+            } ?: Util.sendMessage(event, "Admin channel has not been set yet!")
+        },
+        listOf(
+            set
+        )
+    )
+
     override val names: List<String>
         get() = listOf("adminchannel", "ac")
     override val shortHelpText: String
         get() = "set/get admin channel"
     override val longHelpText: String
-        get() = "`$commandExample`: Sends message to currently set admin channel.\n" +
-                "`$commandExample set`: Sets the current channel as admin channel."
+        get() = adminchannel.toTree().toString()
     override val needsAdmin: Boolean
         get() = true
     private lateinit var kord: Kord
@@ -28,13 +47,6 @@ object AdminChannelCommand : MessageCommandInterface
 
     override suspend fun execute(event: MessageCreateEvent, args: List<String>)
     {
-        if (args.size <= 1) Config.get("adminChannel")?.let { channelID ->
-            Util.sendMessage(MessageChannelBehavior(Snowflake(channelID), kord), "This is the admin channel.")
-        } ?: Util.sendMessage(event, "Admin channel has not been set yet!")
-        else if (args[1] == "set")
-        {
-            Config.store("adminChannel", event.message.channel.id.toString())
-            Util.sendMessage(event, "Admin channel set!")
-        }
+        if (names.contains(args[0].substring(1))) adminchannel.execute(event, args.subList(1, args.size), listOf(args[0].substring(1)))
     }
 }
