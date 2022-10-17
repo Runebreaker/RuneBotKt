f37bc58894ffe75158ca1321eedaec9998528d68
s361591 <gino.schulze-skyfire@web.de>
2022-10-17 20:40:29 +0200

    Added DoggoBehaviour.kt

---
+++ src/main/kotlin/de/runebot/behaviors/DoggoBehaviour.kt
@@ -0,0 +1,17 @@
+package de.runebot.behaviors
+
+import de.runebot.Util
+import dev.kord.core.event.message.MessageCreateEvent
+
+object DoggoBehaviour : Behavior
+{
+    val doggoGif = "https://media.discordapp.net/attachments/824357369559121920/914912078626693130/656954222046347274.gif"
+
+    override suspend fun run(content: String, messageCreateEvent: MessageCreateEvent)
+    {
+        if (doggoGif in content)
+        {
+            Util.sendMessage(messageCreateEvent, doggoGif)
+        }
+    }
+}
