14c515872846ca290ec21f99cd34c4d98ed2b2a4
s361591 <gino.schulze-skyfire@web.de>
2023-02-04 01:41:38 +0100

    Registered new Command.

--- src/main/kotlin/de/runebot/Util.kt
+++ src/main/kotlin/de/runebot/Util.kt
@@ -2,9 +2,12 @@ package de.runebot
 
 import dev.kord.common.Color
 import dev.kord.common.entity.Snowflake
+import dev.kord.common.entity.optional.Optional
+import dev.kord.common.entity.optional.OptionalInt
 import dev.kord.common.exception.RequestException
 import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.cache.data.*
 import dev.kord.core.entity.Message
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.exception.EntityNotFoundException
@@ -13,10 +16,10 @@ import dev.kord.rest.builder.message.EmbedBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.datetime.Clock
-import java.io.FileOutputStream
 import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
 import java.io.InputStream
 import java.net.URL
 import java.nio.channels.Channels
@@ -174,6 +177,24 @@ object Util
         event.message.channel.createMessage { embeds.add(someEmbed) }
     }
 
+    fun replaceUsingRuleset(input: String, ruleset: MutableSet<Rule>): Pair<String, Int>
+    {
+        var result = input
+        var replacementCount = 0
+
+        ruleset.forEach { rule ->
+            val ruleRegex = Regex(rule.regex)
+            ruleRegex.find(input)?.let {
+                result = ruleRegex.replace(result, rule.replace)
+                replacementCount++
+            }
+        }
+
+        return Pair(result, replacementCount)
+    }
+
+    data class Rule(val regex: String = "", val replace: String = "")
+
     class StringTree(rootString: String)
     {
         companion object
@@ -256,6 +277,172 @@ object Util
                 val returnString = final.toString()
                 final.clear()
                 return returnString
+            }
+        }
+    }
+
+    /**
+     * Complete class for paginated Embeds.
+     */
+    class EmbedCatalogue
+    {
+        val catalogue: Catalogue = Catalogue()
+
+        /**
+         * Adds a page to the catalogue and returns it for further modification.
+         * @return The created CataloguePage.
+         * @param atIndex (Optional) Specify at which index the page should be inserted - always appends on default.
+         */
+        fun addPage(atIndex: Int = catalogue.pages.size): CataloguePage
+        {
+            // Should not throw an exception, since the list is always expanded first
+            catalogue.pages.add(CataloguePage())
+            return catalogue.pages.last()
+        }
+
+        /**
+         * Pagination system for CataloguePages.
+         */
+        data class Catalogue(val pages: MutableList<CataloguePage> = mutableListOf(), var index: Int = 0)
+        {
+            fun currentPage(): CataloguePage
+            {
+                return pages[index]
+            }
+
+            fun nextPage(): CataloguePage
+            {
+                if (index < pages.lastIndex) return pages[++index]
+                return currentPage()
+            }
+
+            fun previousPage(): CataloguePage
+            {
+                if (index > 0) return pages[--index]
+                return currentPage()
+            }
+
+            fun gotoPage(desiredIndex: Int): CataloguePage
+            {
+                if (desiredIndex in 0..pages.lastIndex) index = desiredIndex
+                return currentPage()
+            }
+        }
+
+        /**
+         * A wrapper for EmbedBuilder which also part of the implementation of a pagination system for easy use of discord embeds.
+         */
+        data class CataloguePage(val embedBuilder: EmbedBuilder = EmbedBuilder(), val files: MutableMap<String, Path> = mutableMapOf())
+        {
+            var data = EmbedData(
+                title = Optional.Missing(),
+                type = Optional.Missing(),
+                description = Optional.Missing(),
+                url = Optional.Missing(),
+                timestamp = Optional.Missing(),
+                color = OptionalInt.Value(Color(255, 0, 0).rgb),
+                footer = Optional.Missing(),
+                image = Optional.Missing(),
+                thumbnail = Optional.Missing(),
+                video = Optional.Missing(),
+                provider = Optional.Missing(),
+                author = Optional.Missing(),
+                fields = Optional.Missing()
+            )
+
+            fun setTitle(title: String)
+            {
+                data = data.copy(title = Optional.Value(title))
+            }
+
+            fun setDescription(text: String)
+            {
+                data = data.copy(description = Optional.Value(text))
+            }
+
+            fun setURL(url: String)
+            {
+                data = data.copy(url = Optional.Value(url))
+            }
+
+            fun setColor(r: Int, g: Int, b: Int)
+            {
+                data = data.copy(color = OptionalInt.Value(Color(r, g, b).rgb))
+            }
+
+            fun setFooter(text: String, iconPath: Path)
+            {
+                val footerData = EmbedFooterData(
+                    text = text,
+                    iconUrl = Optional.Value("attachment://${iconPath.fileName}"),
+                    proxyIconUrl = Optional.Missing()
+                )
+                data = data.copy(footer = Optional.Value(footerData))
+                files["Icon"] = iconPath
+            }
+
+            fun setImage(path: Path)
+            {
+                val imageData = EmbedImageData(
+                    url = Optional.Value("attachment://${path.fileName}"),
+                    proxyUrl = Optional.Missing(),
+                    height = OptionalInt.Missing,
+                    width = OptionalInt.Missing
+                )
+                data = data.copy(image = Optional.Value(imageData))
+                files["Image"] = path
+            }
+
+            fun setThumbnail(path: Path)
+            {
+                val thumbnailData = EmbedThumbnailData(
+                    url = Optional.Value("attachment://${path.fileName}"),
+                    proxyUrl = Optional.Missing(),
+                    height = OptionalInt.Missing,
+                    width = OptionalInt.Missing
+                )
+                data = data.copy(thumbnail = Optional.Value(thumbnailData))
+                files["Thumbnail"] = path
+            }
+
+            fun setVideo(url: String)
+            {
+                val videoData = EmbedVideoData(
+                    url = Optional.Value(url),
+                    height = OptionalInt.Missing,
+                    width = OptionalInt.Missing
+                )
+                data = data.copy(video = Optional.Value(videoData))
+            }
+
+            fun setProvider(name: String, url: String = "")
+            {
+                val providerData = EmbedProviderData(
+                    name = Optional.Value(name),
+                    url = if (url.isNotBlank()) Optional.Value(url) else Optional.Missing()
+                )
+                data = data.copy(provider = Optional.Value(providerData))
+            }
+
+            fun setAuthor(author: String, url: String = "")
+            {
+                val authorData = EmbedAuthorData(
+                    name = Optional.Value(author),
+                    url = if (url.isNotBlank()) Optional.Value(url) else Optional.Missing(),
+                    iconUrl = Optional.Missing(),
+                    proxyIconUrl = Optional.Missing(),
+                )
+                data = data.copy(author = Optional.Value(authorData))
+            }
+
+            fun addFields(vararg field: EmbedFieldData)
+            {
+                val fields = mutableListOf<EmbedFieldData>()
+                data.fields.value?.let {
+                    fields.addAll(it)
+                }
+                fields.addAll(field)
+                data = data.copy(fields = Optional.Value(fields))
             }
         }
     }
