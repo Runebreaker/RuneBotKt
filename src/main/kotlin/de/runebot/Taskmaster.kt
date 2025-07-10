2a6edf8a6bd4b346a446af559301cfafc32ad03b
Jagenka <dev@jayhannes.de>
2025-07-10 13:58:56 +0200

    fix remove invalid timer

--- src/main/kotlin/de/runebot/Taskmaster.kt
+++ src/main/kotlin/de/runebot/Taskmaster.kt
@@ -6,6 +6,7 @@ import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.entity.ReactionEmoji
 import dev.kord.x.emoji.Emojis
 import kotlinx.coroutines.delay
+import kotlinx.datetime.Instant
 
 object Taskmaster
 {
@@ -28,19 +29,28 @@ object Taskmaster
         RuneBot.kord?.let { kord ->
             timers.filter {
                 it.targetTime <= System.currentTimeMillis()
-            }.forEach { (_, message, channelId, messageId) ->
-                val channel = MessageChannelBehavior(Snowflake(channelId), kord)
-                val channelMessage = channel.getMessage(Snowflake(messageId))
-                val builtMessage: StringBuilder = StringBuilder(message)
-                builtMessage.append(System.lineSeparator())
-                channelMessage.author?.let {
-                    builtMessage.append(it.mention).append(" ")
+            }.forEach { (targetTime, message, channelId, messageId) ->
+                try
+                {
+                    val channel = MessageChannelBehavior(Snowflake(channelId), kord)
+                    val channelMessage = channel.getMessage(Snowflake(messageId))
+                    val builtMessage: StringBuilder = StringBuilder(message)
+                    builtMessage.append(System.lineSeparator())
+                    channelMessage.author?.let {
+                        builtMessage.append(it.mention).append(" ")
+                    }
+                    channelMessage.getReactors(ReactionEmoji.Unicode(Emojis.alarmClock.unicode)).collect {
+                        if (it.isBot || builtMessage.contains(it.mention)) return@collect
+                        builtMessage.append(it.mention).append(" ")
+                    }
+                    Util.sendMessage(channel, builtMessage.toString())
+
+                } catch (exception: Exception)
+                {
+                    exception.printStackTrace()
+                    System.err.println("Timer $message at ${Instant.fromEpochMilliseconds(targetTime)} triggered with an exception and will be removed.")
                 }
-                channelMessage.getReactors(ReactionEmoji.Unicode(Emojis.alarmClock.unicode)).collect {
-                    if (it.isBot || builtMessage.contains(it.mention)) return@collect
-                    builtMessage.append(it.mention).append(" ")
-                }
-                Util.sendMessage(channel, builtMessage.toString())
+
                 DB.removeTimer(channelId, messageId)
                 updateTimers()
             }
